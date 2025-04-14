4ff0e24383d8d36ac784ae46c32c28312954124e
DylanoSpks <dylanored@yahoo.com>
2025-04-14 14:11:37 +0200

    Trigger Detection made
    Triggers (walking through a hidden cube) now makes a E appear.

---
+++ Year 1 Project 3/Assets/Code/TriggerDetecter.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class TriggerDetect : MonoBehaviour
+{
+ public GameObject target;
+ void OnTriggerEnter(Collider other)
+ {
+  target = this.gameObject.transform.GetChild(0).gameObject;
+  target.SetActive(true);
+  Debug.Log("Object Entered the trigger");
+ }
+
+ void OnTriggerStay(Collider other)
+ {
+  Debug.Log("Object is within trigger");
+ }
+
+ void OnTriggerExit(Collider other)
+ {
+  target = this.gameObject.transform.GetChild(0).gameObject;
+  target.SetActive(false);
+  Debug.Log ("object Exited the trigger");
+ }
+}
+
