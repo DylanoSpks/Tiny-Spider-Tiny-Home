c2f2ed35cab2837cdda916e0852f0b1103ee4021
DylanoSpks <dylanored@yahoo.com>
2025-06-03 15:12:53 +0200

    Tutorial

--- Year 1 Project 3/Assets/Code/UI/MainMenu.cs
+++ Year 1 Project 3/Assets/Code/UI/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(0);
+        SceneManager.LoadSceneAsync(4);
     }
 
     public void StartupScreen()
