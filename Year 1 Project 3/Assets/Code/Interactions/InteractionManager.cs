ec18bec7372c8cfd07308e26628c0f86076ba704
lvmbrxu <vladlambru123@gmail.com>
2025-06-09 22:02:25 +0200

    yea

--- Year 1 Project 3/Assets/Code/Interactions/InteractionManager.cs
+++ Year 1 Project 3/Assets/Code/Interactions/InteractionManager.cs
@@ -16,7 +16,8 @@ public class InteractionManager : MonoBehaviour
     [SerializeField] private GameObject promptUI;
     [SerializeField] private TextMeshProUGUI keyText;
     [SerializeField] private TextMeshProUGUI actionText;
-    [SerializeField] private float interactDistance = 3.5f;
+    [SerializeField, Tooltip("Max distance at which you can interact")]
+    private float interactDistance = 3.5f;
     [SerializeField] private KeyCode interactKey = KeyCode.E;
 
     private IInteractable _currentInteractable;
@@ -25,7 +26,8 @@ public class InteractionManager : MonoBehaviour
     private void Awake()
     {
         if (Instance == null) Instance = this;
-        else { Destroy(gameObject); return; }
+        else { Destroy(gameObject);
+        }
     }
 
     private void Start()
@@ -58,22 +60,37 @@ public class InteractionManager : MonoBehaviour
     private void CheckForInteractable()
     {
         Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
         if (Physics.Raycast(ray, out RaycastHit hit, interactDistance))
         {
-            var invis = hit.collider.GetComponent<InvisibilityInteractable>();
-            if (invis != null)
+            float actualDist = Vector3.Distance(playerCamera.transform.position, hit.collider.transform.position);
+            if (actualDist <= interactDistance)
             {
-                _currentInteractable = invis;
-                _currentInteractableObject = hit.collider.gameObject;
-                promptUI.SetActive(true);
-                return;
+                var invis = hit.collider.GetComponent<InvisibilityInteractable>();
+                if (invis != null)
+                {
+                    _currentInteractable = invis;
+                    _currentInteractableObject = hit.collider.gameObject;
+                    promptUI.SetActive(true);
+                    return;
+                }
             }
         }
+
         if (_currentInteractable != null)
         {
             _currentInteractable = null;
             _currentInteractableObject = null;
             promptUI.SetActive(false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (playerCamera != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(playerCamera.transform.position, interactDistance);
         }
     }
 }
